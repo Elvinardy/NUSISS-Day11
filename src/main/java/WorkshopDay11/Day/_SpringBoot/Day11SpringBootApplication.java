046061464a5201d52ed0954330c933ff43e8848c
Elvinardy Darwin <elvinardy_darwin@Elvinardys-MacBook-Pro.local>
2022-01-05 13:54:33 +0800

    first commit

---
+++ src/main/java/WorkshopDay11/Day/_SpringBoot/Day11SpringBootApplication.java
@@ -0,0 +1,13 @@
+package WorkshopDay11.Day._SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day11SpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day11SpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/WorkshopDay11/Day/_SpringBoot/Day11SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package WorkshopDay11.Day._SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day11SpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
